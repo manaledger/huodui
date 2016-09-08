248549860f6c3d665a8dca1a9830f6f7d985ddd3
liujiaxi <liujiaxi@mana.cn>
2016-09-08 20:38:11 +0800

    add help information for message reciver worker script
    为消息接受脚本添加命令行帮助信息。

--- script/mqreceive.js
+++ script/mqreceive.js
@@ -2,11 +2,20 @@ const mqconfig = require('./mqconfig');
 var basename = require('path').basename;
 var open = require('amqplib').connect(mqconfig.url);
 const BlockChain = require('./jsondata');
+const yargs = require('yargs');
 
-var keys = process.argv.slice(2);
-if (keys.length < 2){
-  console.info("usage: node %s [quque] [key.pattern]", basename(process.argv[1]));
-}
+var argv = require('yargs')
+    .usage('Usage: node $0  [queue] [routekey]')
+    .example('node $0  json  json.huodui', 'the json queue will receive message that router key is json.huodui')
+    .alias('q', 'queue')
+    .nargs('q', 1)
+    .describe('q', 'message quque name')
+    .alias('k', 'key')
+    .nargs('k', 1)
+    .describe('k', 'message router key')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
 
 var channel, queue;
 var blockchain = new BlockChain();
@@ -21,22 +30,26 @@ function startWorker() {
     channel.on('close', handleClose);
     return ch.assertExchange(mqconfig.exchange, 'topic', {durable:true});
   }).then(function () {
-    return channel.assertQueue(keys[0] || mqconfig.defaultQueue, {durable:true});
+    return channel.assertQueue(argv.queue || mqconfig.defaultQueue, {durable:true});
   }).then(function (q) {
     queue = q.queue;
     channel.prefetch(1);
-    return channel.bindQueue(queue, mqconfig.exchange, keys[1] || mqconfig.defaultKey);
+    return channel.bindQueue(queue, mqconfig.exchange, argv.key || mqconfig.defaultKey);
   }).then(function () {
     return channel.consume(queue, function (msg) {
-      var obj = JSON.parse(msg.content.toString());
-      return blockchain.saveJson(obj.key, JSON.stringify(obj.json)).then(function () {
-        console.log("[x] %s %s", msg.fields.routingKey, msg.content.toString());
+      try {
+        var obj = JSON.parse(msg.content.toString());
+        return blockchain.saveJson(obj.key, JSON.stringify(obj.json)).then(function () {
+          console.log("[x] %s %s", msg.fields.routingKey, msg.content.toString());
+          channel.ack(msg);
+        }).catch(function (err) {
+          channel.ack(msg);
+          console.warn(err);
+        });
+      } catch (e) {
+        console.log("msg %s: ", msg.content.toString());
         channel.ack(msg);
-      }).catch(function (err) {
-        channel.ack(msg);
-        console.warn(err);
-      });
-
+      }
     }, {noAck:false});
   }).catch(console.warn);
 }
@@ -47,6 +60,7 @@ function handleError(err) {
 
 function handleClose() {
   console.info('connetion closed.');
+  startWorker();
 }
 
 startWorker();
